4582431c78313197e80434bd305934d78d924a4f
Aytuğ Sezer <aytugsezer@gmail.com>
2026-07-10 22:51:30 +0300

    chore: update showagent to v0.11.0

--- Formula/showagent.rb
+++ Formula/showagent.rb
@@ -4,26 +4,26 @@
 class Showagent < Formula
   desc "Browse, resume, branch, and convert local AI coding sessions in one TUI"
   homepage "https://github.com/aytzey/showagent"
-  version "0.10.0"
+  version "0.11.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/aytzey/showagent/releases/download/v0.10.0/showagent_v0.10.0_darwin_arm64.tar.gz"
-      sha256 "4a32d93f99506b30287bf80c5aff2c9232f5f7ead5d7ea2130075809e095b6ea"
+      url "https://github.com/aytzey/showagent/releases/download/v0.11.0/showagent_v0.11.0_darwin_arm64.tar.gz"
+      sha256 "cd5e903a891a2b37b88d31e4f3ec07d41615996c729603f97fc44df926b3886a"
     else
-      url "https://github.com/aytzey/showagent/releases/download/v0.10.0/showagent_v0.10.0_darwin_amd64.tar.gz"
-      sha256 "ab205aab0ea10fe9ec20d93950ac059215a2c270d018fe20f2c27210517335a9"
+      url "https://github.com/aytzey/showagent/releases/download/v0.11.0/showagent_v0.11.0_darwin_amd64.tar.gz"
+      sha256 "19e165af31d218e9a0f9a9e4e81ff7eef02b9d6c614c29363a9994687f9607e3"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/aytzey/showagent/releases/download/v0.10.0/showagent_v0.10.0_linux_arm64.tar.gz"
-      sha256 "bb7f716cf15411f2a56a3f1a3adb285d577c6580d1d44b32869fa454c59efca5"
+      url "https://github.com/aytzey/showagent/releases/download/v0.11.0/showagent_v0.11.0_linux_arm64.tar.gz"
+      sha256 "2d2a576f1eea03ec3d7564dc5cb1cd85b89561b43afadbcd592c4834ad30da81"
     else
-      url "https://github.com/aytzey/showagent/releases/download/v0.10.0/showagent_v0.10.0_linux_amd64.tar.gz"
-      sha256 "2bec3e0945f442099c32f7ed8cb6a1498bc9247ec1b24574f3e7d279e6a5035b"
+      url "https://github.com/aytzey/showagent/releases/download/v0.11.0/showagent_v0.11.0_linux_amd64.tar.gz"
+      sha256 "a5ff4f31b161fcb2a5321cecbf1b5ae8fc6a62da831198ed738912571d1f76b8"
     end
   end
 
